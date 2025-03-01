92fcc10ae521aea88e8bebca087e95c6e2e017d2
HienKoii <khien1892002@gmail.com>
2025-03-01 23:52:51 +0700

    sua db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,26 +1,14 @@
 import mysql from "mysql2/promise";
 
+const databaseUrl = new URL(process.env.DATABASE_URL);
+
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: databaseUrl.hostname,
+  user: databaseUrl.username,
+  password: databaseUrl.password,
+  database: databaseUrl.pathname.replace("/", ""), // Bỏ dấu '/'
+  port: databaseUrl.port || 3306, // Mặc định MySQL dùng cổng 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
-async function checkConnection() {
-  try {
-    // Kiểm tra kết nối với database
-    const connection = await db.getConnection();
-    console.log("Kết nối tới MySQL thành công!");
-    connection.release(); // Giải phóng kết nối sau khi sử dụng
-  } catch (error) {
-    console.error("Lỗi khi kết nối tới MySQL:", error.message);
-    console.error("Chi tiết lỗi:", error);
-  }
-}
-
-// Gọi hàm kiểm tra kết nối khi khởi động ứng dụng
-checkConnection();
