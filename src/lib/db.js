aa7b6eb7d72caca1eb68ea92d2472eaad7214644
HienKoii <khien1892002@gmail.com>
2026-04-05 23:14:24 +0700

    thêm check kết nối dtb

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,6 +5,21 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+// Hàm kiểm tra kết nối
+export async function checkDBConnection() {
+  try {
+    const connection = await db.getConnection();
+    console.log("✅ Kết nối database thành công!");
+    connection.release();
+  } catch (error) {
+    console.error("❌ Kết nối database thất bại!");
+    console.error(error.message);
+  }
+}
 
 export default db;
