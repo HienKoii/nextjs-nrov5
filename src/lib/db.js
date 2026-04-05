42c928c22a1ef0a012213f3ade52894e36ead889
HienKoii <khien1892002@gmail.com>
2026-04-05 23:44:11 +0700

    sửa db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,6 @@
 import mysql from "mysql2/promise";
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 // Hàm kiểm tra kết nối
 export async function checkDBConnection() {
